7d7bcfb47c347dabbb1b2ed105db17b4d82e5582
Daniel Beauchamp <daniel.beauchamp@shopify.com>
2017-02-02 12:10:21 -0500

    Initial commit

---
+++ Source/Marionette/MarionetteGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MarionetteGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARIONETTE_API AMarionetteGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
